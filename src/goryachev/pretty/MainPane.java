de32d9bd6b7a5b9836975b4b8072194832209977
Andy Goryachev <andy@goryachev.com>
2017-06-10 09:30:08 -0700

    main pane

---
+++ src/goryachev/pretty/MainPane.java
@@ -0,0 +1,176 @@
+// Copyright © 2017 Andy Goryachev <andy@goryachev.com>
+package goryachev.pretty;
+import goryachev.common.util.CKit;
+import goryachev.common.util.CList;
+import goryachev.common.util.Log;
+import goryachev.fx.CAction;
+import goryachev.fx.CBooleanProperty;
+import goryachev.fx.CCheckBox;
+import goryachev.fx.CPane;
+import goryachev.fx.FX;
+import goryachev.pretty.analysis.AnalysisReport;
+import goryachev.pretty.analysis.Base64Analyzer;
+import goryachev.pretty.analysis.HexAnalyzer;
+import goryachev.pretty.analysis.IntegerAnalyzer;
+import goryachev.pretty.format.PrettyFormatter;
+import goryachev.pretty.parser.ParseResult;
+import goryachev.pretty.parser.RecursiveJsonXmlParser;
+import goryachev.pretty.parser.Segment;
+import goryachev.pretty.parser.Type;
+import goryachev.pretty.view.ContentView;
+import java.util.List;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.geometry.Orientation;
+import javafx.scene.control.SplitPane;
+import javafx.scene.input.Clipboard;
+import javafx.util.Duration;
+
+
+/**
+ * Main Pane.
+ */
+public class MainPane
+	extends CPane
+{
+	public final CAction copyAction = new CAction(this::copy); 
+	public final CAction pasteAction = new CAction(this::pasteFromClipboard);
+	public final CCheckBox monitorClipboardCheckbox;
+	public final ContentView view;
+	public final DetailPane detailPane;
+	public final SplitPane split;
+	protected final SimpleBooleanProperty horizontalSplit = new SimpleBooleanProperty(true);
+	protected static final Duration PERIOD = Duration.millis(200);
+	protected final Clipboard clipboard;
+	protected final CBooleanProperty monitorClipboardProperty = new CBooleanProperty(true, this::updateClipboardMonitoring);
+	private Timeline timeline;
+	private String oldContent;
+
+	
+	public MainPane()
+	{
+		view = new ContentView();
+		
+		detailPane = new DetailPane();
+		
+		view.caretSpotProperty().addListener((src) -> updateDetailPane());
+		
+		split = new SplitPane(view.getNode(), detailPane);
+		split.setOrientation(Orientation.HORIZONTAL);
+		split.setDividerPositions(0.8);
+
+		setCenter(split);
+		
+		// clipboard
+		clipboard = Clipboard.getSystemClipboard();
+		monitorClipboardCheckbox = new CCheckBox("monitor clipboard");
+		monitorClipboardCheckbox.selectedProperty().bindBidirectional(monitorClipboardProperty);
+		FX.listen(this::updateSplit, true, horizontalSplit);
+	}
+	
+	
+	protected void updateSplit()
+	{
+		boolean hor = horizontalSplit.get();
+		split.setOrientation(hor ? Orientation.HORIZONTAL : Orientation.VERTICAL);
+	}
+	
+	
+	protected void updateDetailPane()
+	{
+		CaretSpot sp = view.caretSpotProperty().get();
+		AnalysisReport rep = analyze(sp.getPosition(), sp.getText());
+		detailPane.setReport(rep);
+	}
+
+
+	protected AnalysisReport analyze(int pos, String text)
+	{
+		AnalysisReport rep = new AnalysisReport();
+		
+		// analyzers
+		new Base64Analyzer(pos, text).report(rep);
+		new HexAnalyzer(pos, text).report(rep);
+		new IntegerAnalyzer(pos, text).report(rep);
+		
+		return rep;
+	}
+	
+	
+	public void updateContent(String s)
+	{
+		List<Segment> ss = parseAndFormat(s);
+		view.setTextSegments(ss);
+		detailPane.clear();
+	}
+	
+
+	protected List<Segment> parseAndFormat(String text)
+	{
+		if(text == null)
+		{
+			return new CList();
+		}
+		
+		try
+		{
+			// parse
+			ParseResult r = new RecursiveJsonXmlParser(text).parse();
+			List<Segment> segments = r.getSegments();
+			
+			// format
+			PrettyFormatter f = new PrettyFormatter(segments);
+			// TODO set options
+			CList<Segment> formatted = f.format();
+			return formatted;
+		}
+		catch(Exception e)
+		{
+			Log.ex(e);
+			return new CList<Segment>(new Segment(Type.ERROR, CKit.stackTrace(e)));
+		}
+	}
+	
+	
+	public void copy()
+	{
+		view.copy();
+	}
+
+	
+	protected void pasteFromClipboard()
+	{
+		if(clipboard.hasString())
+		{
+			String s = clipboard.getString();
+			if(CKit.notEquals(oldContent, s))
+			{
+				updateContent(s);
+				oldContent = s;
+			}
+		}
+	}
+	
+	
+	protected void updateClipboardMonitoring()
+	{
+		if(monitorClipboardProperty.get())
+		{
+			if(timeline == null)
+			{
+				timeline = new Timeline(new KeyFrame(PERIOD, (ev) -> pasteFromClipboard()));
+				timeline.setCycleCount(Timeline.INDEFINITE);
+				timeline.play();
+			}
+		}
+		else
+		{
+			if(timeline != null)
+			{
+				timeline.stop();
+				timeline = null;
+			}
+		}
+	}
+}
--- src/goryachev/pretty/MainWindow.java
+++ src/goryachev/pretty/MainWindow.java
@@ -1,13 +1,6 @@
 // Copyright © 2017 Andy Goryachev <andy@goryachev.com>
 package goryachev.pretty;
-import goryachev.common.util.CKit;
-import goryachev.common.util.CList;
-import goryachev.common.util.D;
-import goryachev.common.util.Log;
-import goryachev.fx.CAction;
-import goryachev.fx.CBooleanProperty;
 import goryachev.fx.CButton;
-import goryachev.fx.CCheckBox;
 import goryachev.fx.CCheckMenuItem;
 import goryachev.fx.CMenu;
 import goryachev.fx.CMenuBar;
@@ -16,27 +9,9 @@ import goryachev.fx.FX;
 import goryachev.fx.FxDump;
 import goryachev.fx.FxWindow;
 import goryachev.fx.HPane;
-import goryachev.pretty.analysis.AnalysisReport;
-import goryachev.pretty.analysis.Base64Analyzer;
-import goryachev.pretty.analysis.HexAnalyzer;
-import goryachev.pretty.analysis.IntegerAnalyzer;
-import goryachev.pretty.format.PrettyFormatter;
-import goryachev.pretty.parser.ParseResult;
-import goryachev.pretty.parser.RecursiveJsonXmlParser;
-import goryachev.pretty.parser.Segment;
-import goryachev.pretty.parser.Type;
-import goryachev.pretty.view.ContentView;
-import java.util.List;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.beans.property.SimpleBooleanProperty;
 import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.scene.Node;
-import javafx.scene.control.SplitPane;
-import javafx.scene.input.Clipboard;
 import javafx.scene.paint.Color;
-import javafx.util.Duration;
 
 
 /**
@@ -45,51 +20,26 @@ import javafx.util.Duration;
 public class MainWindow
 	extends FxWindow
 {
-	public final ContentView view;
-	public final DetailPane detailPane;
-	public final SplitPane split;
-	protected static final Duration PERIOD = Duration.millis(200);
-	protected final Clipboard clipboard;
-	protected String oldContent;
-	protected final CBooleanProperty monitorClipboardProperty = new CBooleanProperty(true, this::updateClipboardMonitoring);
-	protected final CCheckBox monitorClipboardCheckbox;
-	protected final SimpleBooleanProperty horizontalSplit = new SimpleBooleanProperty(true);
-	public final CAction copyAction = new CAction(this::copy); 
-	public final CAction pasteAction = new CAction(this::pasteFromClipboard);
-	private Timeline timeline;
+	public final MainPane pane;
 	
 	
 	public MainWindow()
 	{
 		super("MainWindow");
-				
-		view = new ContentView();
 		
-		detailPane = new DetailPane();
-		
-		monitorClipboardCheckbox = new CCheckBox("monitor clipboard");
-		monitorClipboardCheckbox.selectedProperty().bindBidirectional(monitorClipboardProperty);
-		
-		view.caretSpotProperty().addListener((src) -> updateDetailPane());
-		
-		split = new SplitPane(view.getNode(), detailPane);
-		split.setOrientation(Orientation.HORIZONTAL);
-		split.setDividerPositions(0.8);
-		
+		pane = new MainPane();
+								
 		setTitle("Pretty Print JSON/XML " + Version.VERSION);
 		setTop(createMenu());
-		setCenter(split);
+		setCenter(pane);
 		setBottom(createStatusBar());
 		setSize(600, 700);
 
-		clipboard = Clipboard.getSystemClipboard();
-		
 		// preferences
-		bind("HSPLIT", horizontalSplit);
-		FX.listen(this::updateSplit, true, horizontalSplit);
-		bind("MONITOR_CLIPBOARD", monitorClipboardProperty);
+		bind("HSPLIT", pane.horizontalSplit);
+		bind("MONITOR_CLIPBOARD", pane.monitorClipboardProperty);
 		
-		updateClipboardMonitoring();
+		pane.updateClipboardMonitoring();
 
 		// debugging
 		FxDump.attach(this);
@@ -109,16 +59,16 @@ public class MainWindow
 		m.add("Quit", FX.exitAction());
 		// edit
 		mb.add(m = new CMenu("Edit"));
-		m.add("Copy", copyAction);
+		m.add("Copy", pane.copyAction);
 		m.add("Save Selection As...");
 		m.separator();
-		m.add("Select All", view.textField.selectAllAction);
+		m.add("Select All", pane.view.textField.selectAllAction);
 		m.separator();
-		m.add(new CCheckMenuItem("Monitor Clipboard", monitorClipboardProperty));
-		m.add("Paste from Clipboard", pasteAction);
+		m.add(new CCheckMenuItem("Monitor Clipboard", pane.monitorClipboardProperty));
+		m.add("Paste from Clipboard", pane.pasteAction);
 		// view
 		mb.add(m = new CMenu("View"));
-		m.add(new CCheckMenuItem("Detail Pane on a Side", horizontalSplit));
+		m.add(new CCheckMenuItem("Detail Pane on a Side", pane.horizontalSplit));
 		// help
 		mb.add(m = new CMenu("Help"));
 		m.add("About");
@@ -138,8 +88,8 @@ public class MainWindow
 			CPane.PREF
 		);
 		p.add(0, 0, mb);
-		p.add(1, 0, monitorClipboardCheckbox);
-		p.add(2, 0, new CButton("Paste", pasteAction));
+		p.add(1, 0, pane.monitorClipboardCheckbox);
+		p.add(2, 0, new CButton("Paste", pane.pasteAction));
 		
 		return p;
 	}
@@ -151,110 +101,5 @@ public class MainWindow
 		p.fill();
 		p.add(FX.label("copyright © 2017 andy goryachev", Color.GRAY, new Insets(1, 10, 1, 2)));
 		return p;
-	}
-	
-	
-	protected void updateSplit()
-	{
-		boolean hor = horizontalSplit.get();
-		split.setOrientation(hor ? Orientation.HORIZONTAL : Orientation.VERTICAL);
-	}
-	
-	
-	protected void updateContent(String s)
-	{
-		List<Segment> ss = parseAndFormat(s);
-		view.setTextSegments(ss);
-		detailPane.clear();
-	}
-
-
-	protected List<Segment> parseAndFormat(String text)
-	{
-		if(text == null)
-		{
-			return new CList();
-		}
-		
-		try
-		{
-			// parse
-			ParseResult r = new RecursiveJsonXmlParser(text).parse();
-			List<Segment> segments = r.getSegments();
-			
-			// format
-			PrettyFormatter f = new PrettyFormatter(segments);
-			// TODO set options
-			CList<Segment> formatted = f.format();
-			return formatted;
-		}
-		catch(Exception e)
-		{
-			Log.ex(e);
-			return new CList<Segment>(new Segment(Type.ERROR, CKit.stackTrace(e)));
-		}
-	}
-	
-	
-	protected void updateDetailPane()
-	{
-		CaretSpot sp = view.caretSpotProperty().get();
-		AnalysisReport rep = analyze(sp.getPosition(), sp.getText());
-		detailPane.setReport(rep);
-	}
-
-
-	protected AnalysisReport analyze(int pos, String text)
-	{
-		AnalysisReport rep = new AnalysisReport();
-		
-		// analyzers
-		new Base64Analyzer(pos, text).report(rep);
-		new HexAnalyzer(pos, text).report(rep);
-		new IntegerAnalyzer(pos, text).report(rep);
-		
-		return rep;
-	}
-	
-	
-	protected void copy()
-	{
-		view.copy();
-	}
-	
-	
-	protected void pasteFromClipboard()
-	{
-		if(clipboard.hasString())
-		{
-			String s = clipboard.getString();
-			if(CKit.notEquals(oldContent, s))
-			{
-				updateContent(s);
-				oldContent = s;
-			}
-		}
-	}
-	
-	
-	protected void updateClipboardMonitoring()
-	{
-		if(monitorClipboardProperty.get())
-		{
-			if(timeline == null)
-			{
-				timeline = new Timeline(new KeyFrame(PERIOD, (ev) -> pasteFromClipboard()));
-				timeline.setCycleCount(Timeline.INDEFINITE);
-				timeline.play();
-			}
-		}
-		else
-		{
-			if(timeline != null)
-			{
-				timeline.stop();
-				timeline = null;
-			}
-		}
 	}
 }
