8fcbe228038f7c5d08f31217b867c4949e5fb541
Andy Goryachev <andy@goryachev.com>
2017-06-13 17:05:33 -0700

    rtf copy!

--- src/goryachev/pretty/view/SegmentEditorModel.java
+++ src/goryachev/pretty/view/SegmentEditorModel.java
@@ -25,7 +25,7 @@ public class SegmentEditorModel
 	public SegmentEditorModel()
 	{
 		addClipboardHandler(new HtmlClipboardHandler(this));
-//		addClipboardHandler(new RtfClipboardHandler(this));
+		addClipboardHandler(new RtfClipboardHandler(this));
 	}
 
 
